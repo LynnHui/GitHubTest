dac7e84832339b16261875d0fb96eb208bc5f374
TrafficViolation <lynn@lynnhuideiMac.local>
2015-05-05 16:40:04 +0800

    custom cell

---
+++ GitHubTest/PlayerCell.h
@@ -0,0 +1,13 @@
+//
+//  PlayerCell.h
+//  GitHubTest
+//
+//  Created by lynn.hui on 15/5/5.
+//  Copyright (c) 2015年 lynn.hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerCell : UITableViewCell
+
+@end
---
+++ GitHubTest/PlayerCell.m
@@ -0,0 +1,23 @@
+//
+//  PlayerCell.m
+//  GitHubTest
+//
+//  Created by lynn.hui on 15/5/5.
+//  Copyright (c) 2015年 lynn.hui. All rights reserved.
+//
+
+#import "PlayerCell.h"
+
+@implementation PlayerCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
