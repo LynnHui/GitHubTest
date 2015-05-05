7dfad2f1ae984223266653c2ebc4b14450f20f4d
TrafficViolation <lynn@lynnhuideiMac.local>
2015-05-05 13:40:36 +0800

    init proj

---
+++ GitHubTest/GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by lynn.hui on 15/5/5.
+//  Copyright (c) 2015年 lynn.hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
