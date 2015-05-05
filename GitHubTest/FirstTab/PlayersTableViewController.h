2dc91e65d19ef6ffac919e7be05122cb0e65a276
TrafficViolation <lynn@lynnhuideiMac.local>
2015-05-05 15:25:47 +0800

    modify table dirct

---
+++ GitHubTest/FirstTab/PlayersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayersTableViewController.h
+//  GitHubTest
+//
+//  Created by lynn.hui on 15/5/5.
+//  Copyright (c) 2015年 lynn.hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayersTableViewController : UITableViewController
+@property(nonatomic, strong) NSArray *cellTitles;
+@end
